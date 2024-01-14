69980e2f27177fe9e8f9d0407958dc727e2eccdb
MohamedAlaga <malaga.142014@gmail.com>
2024-01-15 00:03:29 +0200

    base model change

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             models.storage.new(self)
-        self.updated_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string representation of the instance"""
