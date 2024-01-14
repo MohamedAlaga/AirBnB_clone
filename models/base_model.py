ef6d2995e572fdf22195693948db6939b096090c
MohamedAlaga <malaga.142014@gmail.com>
2024-01-14 07:14:47 +0200

    done all

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,11 @@ This module defines a base class for all models in hbnb clone
 import uuid
 from datetime import datetime
 import models
+
+
 class BaseModel:
     """base class for all classes in hbnb clone"""
     def __init__(self, *args, **kwargs):
-        
         """Initialization of the base model"""
         if kwargs:
             for key, value in kwargs.items():
@@ -22,13 +23,17 @@ class BaseModel:
             self.created_at = datetime.now()
             models.storage.new(self)
         self.updated_at = datetime.now()
+
     def __str__(self):
         """returns a string representation of the instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        name = self.__class__.__name__
+        return "[{}] ({}) {}".format(name, self.id, self.__dict__)
+
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
         models.storage.save()
+
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
         new_dict = self.__dict__.copy()
@@ -36,4 +41,3 @@ class BaseModel:
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
-    
